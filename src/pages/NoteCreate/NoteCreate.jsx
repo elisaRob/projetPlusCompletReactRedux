e146fa86b49cd0e7c9a738ed0ba23d7fbe7cd81f
Elisabeth ROBERT AOUCHER <elisabethrobertaoucher@gmail.com>
2024-01-03 17:47:09 +0100

    ajouts

---
+++ src/pages/NoteCreate/NoteCreate.jsx
@@ -0,0 +1,7 @@
+import s from "./style.module.css"
+
+export function NoteCreate(){
+    return <>
+        Note Create
+    </>
+}
---
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -0,0 +1,7 @@
+import s from "./style.module.css"
+
+export function PageNotFound(){
+    return <>
+        404-Page not found
+    </>
+}
